0a3a65fff4eeaf3e91abd1f1f4f9b92583b74164
ndtctk1295 <nguyentuananh190403@gmail.com>
2022-08-15 23:28:52 +0700

    btvnls7

---
+++ btvn13-08-2022 js/index.js
@@ -0,0 +1,6 @@
+// bắt người dùng nhập đầy đủ họ tên và in ra màn hình họ tên người dùng riêng lẻ
+// NguyenTuanAnh
+let tencuaban = prompt("Tên của bạn")
+console.log('tencuanban', tencuaban)
+console.log('Họ', tencuaban.slice(0,6))
+console.log('Tên',tencuaban.slice(6,13))
